cf115c8c7e896e2dd169fa57af1cbcc6251554ec
Finn <derfinn@gmail.com>
2015-06-24 18:54:03 +0200

    add blinking edges

--- example.js
+++ example.js
@@ -11,8 +11,17 @@ graph.connect('B', 'C', 'out', '2')
 var view = new FlowGraphView(graph)
 document.body.appendChild(view.svg)
 
+
+setTimeout(function () {
+  view.blinkEdge('B', 'C')
+}, 2000)
+
+setTimeout(function () {
+  view.blinkEdge('B', 'C')
+}, 4000)
+
 view.on('node-select', function (node) {
-  console.log(node)
+  console.log('Node clicked', node)
 })
 
 insertCss(FlowGraph.css)
