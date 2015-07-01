5fd3f02b41d8b65f6bbbcb4ab1f269b4e9700e6d
Finn <derfinn@gmail.com>
2015-07-01 18:13:27 +0200

    code formatting

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var random = require('randomjs')
 
 module.exports = Flowgraph
 
-function Flowgraph() {
+function Flowgraph () {
   EventEmitter.call(this)
   this.nodes = []
   this.edges = []
@@ -22,12 +22,10 @@ inherits(Flowgraph, EventEmitter)
 
 Flowgraph.css = fs.readFileSync(__dirname + '/style.css').toString()
 
-Flowgraph.View = require('./lib/view')
-
 Flowgraph.prototype.addNode = function (node, inports, outports) {
-  if(typeof inports === 'string') inports = [inports]
-  if(typeof outports === 'string') outports = [outports]
-  
+  if (typeof inports === 'string') inports = [inports]
+  if (typeof outports === 'string') outports = [outports]
+
   var options = {
     id: random('[a-z][a-z0-9_]{16}'),
     x: 0,
@@ -35,29 +33,33 @@ Flowgraph.prototype.addNode = function (node, inports, outports) {
     inports: inports || ['in'],
     outports: outports || ['out']
   }
-  if(typeof node === 'string') options.id = node
+  if (typeof node === 'string') options.id = node
   else options = xtend(options, node)
 
-  if(!/^[a-z][a-z0-9_]*$/.test(options.id)) throw(new Error('ID can only be [a-z][a-z0-9_]+'))
-  if(this.getNode(options.id))  throw(new Error('Node ID already exists'))
+  if (!/^[a-z][a-z0-9_]*$/.test(options.id)) throw (new Error('ID can only be [a-z][a-z0-9_]+'))
+  if (this.getNode(options.id)) throw (new Error('Node ID already exists'))
 
   this.nodes.push(options)
   this.emit('node-added', options)
   return options
 }
 
-Flowgraph.prototype.getInports = function(node) {
+Flowgraph.prototype.addNodes = function (nodes) {
+  nodes.forEach(this.addNode.bind(this))
+}
+
+Flowgraph.prototype.getInports = function (node) {
   return this.getNode(node).inports || []
 }
 
-Flowgraph.prototype.getOutports = function(node) {
+Flowgraph.prototype.getOutports = function (node) {
   return this.getNode(node).outports || []
 }
 
 Flowgraph.prototype.deleteNode = function (id) {
   var removedEdges = []
   this.edges = this.edges.filter(function (edge) {
-    if(edge.source.id === id || edge.target.id === id) {
+    if (edge.source.id === id || edge.target.id === id) {
       removedEdges.push(edge)
       return false
     }
@@ -66,49 +68,62 @@ Flowgraph.prototype.deleteNode = function (id) {
   removedEdges.forEach(function (edge) {
     this.emit('edge-deleted', edge)
   }.bind(this))
-  
+
   var deletedNode = null
   this.nodes = this.nodes.filter(function (node) {
-    if(node.id === id) deletedNode = node
+    if (node.id === id) deletedNode = node
     else return true
   })
-  if(deletedNode) this.emit('node-deleted', deletedNode)
+  if (deletedNode) this.emit('node-deleted', deletedNode)
 }
 
-Flowgraph.prototype.connect = function (source, target, sourcePort, targetPort) {  
+Flowgraph.prototype.connect = function (source, target, sourcePort, targetPort) {
+  if (!sourcePort) sourcePort = this.defaultOutport(source)
+  if (!targetPort) targetPort = this.defaultInport(target)
+
   var edge = {
-    source: {id: source, port: sourcePort || 'in'},
-    target: {id: target, port: targetPort || 'out'}
+    source: {id: source, port: sourcePort},
+    target: {id: target, port: targetPort}
   }
   this.edges.push(edge)
   this.emit('edge-added', edge)
 }
 
-Flowgraph.prototype.disconnect = function (a, b, aPort, bPort) {
+Flowgraph.prototype.disconnect = function (source, target, sourcePort, targetPort) {
   var removedEdge = null
 
   this.edges = this.edges.filter(function (edge) {
-    if(edge.source.port !== aPort || edge.target.port !== bPort) return true
-    if(edge.source.id === a && edge.target.id === b) removedEdge = edge
+    if (edge.source.port !== sourcePort || edge.target.port !== targetPort) return true
+    if (edge.source.id === source && edge.target.id === target) removedEdge = edge
     else return true
   })
-  if(removedEdge) this.emit('edge-deleted', removedEdge)
+  if (removedEdge) this.emit('edge-deleted', removedEdge)
 }
 
-Flowgraph.prototype.getEdges = function() {
+Flowgraph.prototype.getEdges = function () {
   return this.edges
 }
 
-Flowgraph.prototype.getEdge = function (source, target, outport, inport) {
-  inport = inport || 'in'
-  outpot = outport || 'out'
+Flowgraph.prototype.getEdge = function (source, target, sourcePort, targetPort) {
+  if (!sourcePort) sourcePort = this.defaultOutport(source)
+  if (!targetPort) targetPort = this.defaultInport(target)
 
   return this.edges.filter(function (edge) {
-    return edge.source.id === source 
-          && edge.source.port === outport
-          && edge.target.id === target
-          && edge.target.port === inport
+    return edge.source.id === source &&
+          edge.source.port === sourcePort &&
+          edge.target.id === target &&
+          edge.target.port === targetPort
   })[0]
+}
+
+Flowgraph.prototype.defaultOutport = function (node) {
+  var outports = this.getNode(node).outports
+  return outports.length === 1 ? outports[0] : false
+}
+
+Flowgraph.prototype.defaultInport = function (node) {
+  var inports = this.getNode(node).inports
+  return inports.length === 1 ? inports[0] : false
 }
 
 Flowgraph.prototype.getNode = function (id) {
@@ -128,7 +143,7 @@ Flowgraph.prototype.toJSON = function () {
 }
 
 Flowgraph.prototype.import = function (graph) {
-  if(typeof graph === 'string') graph = JSON.parse(graph)
+  if (typeof graph === 'string') graph = JSON.parse(graph)
   // TODO: Validate import
   this.nodes = graph.nodes
   this.edges = graph.edges
